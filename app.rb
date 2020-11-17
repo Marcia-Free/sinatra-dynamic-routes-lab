9272cc866344516297ae12c917dd0d14948cda58
Marcia-Free <MarciaFreeman2011@gmail.com>
2020-11-17 17:52:06 -0600

    commit tree c6df3cb19edc3293d77575a80fddb59d8d52b470

--- app.rb
+++ app.rb
@@ -1,6 +1,45 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  #@user_name = params[:name]
+  #"Goodbye, #{@user_name}."
+
+  get '/reversename/:name' do
+    @reverse_name = params[:name].reverse
+  end
+
+  get '/square/:number' do
+    @num_squared = params[:number].to_i * params[:number].to_i
+    @num_squared.to_s
+  end
+
+  get '/say/:number/:phrase' do
+    @say = ''
+      params[:number].to_i.times { @say += params[:phrase] }
+    @say
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @say = "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+
+  get '/:operation/:number1/:number2' do
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+
+    @answer = 'Unable to perform this operation'
+
+    case params[:operation]
+      when 'add'
+        @answer = (num1 + num2).to_s
+      when 'subtract'
+        @answer = (num1 - num2).to_s
+      when 'multiply'
+        @answer = (num1 * num2).to_s
+      when 'divide'
+        @answer = (num1 / num2).to_s
+    end
+  end
 
 end
